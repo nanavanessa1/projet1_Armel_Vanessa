d9db37ece62ea4ef79c41dfa9f862e43111c6043
vanessa <armelkiyetenanavanessa@gmail.com>
2023-11-22 14:53:01 -0500

    debut du tp

---
+++ loginUp.php
@@ -0,0 +1,12 @@
+<h2>Connexion</h2>
+<a href ="../">Accueil</a>
+<form action="../results/loginResult.php" method="post">
+    
+    <label for ="user_name"> nom d'utilisateur</label>
+    <input id="user_name" type="text" name="user_name" value=""></br>
+    <label for ="email"> Email</label>
+    <input id="email" type="text" name="email" value=""></br>
+    <label for ="pwd"> Password</label>
+    <input id="pwd" type="text" name="pwd" value=""></br>
+    
+    <button type="submit" > Se connecter</button>
